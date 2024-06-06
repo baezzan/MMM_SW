9e7513f6f75fa467518841a00003495a0464b76c
baezzan <139171359+baezzan@users.noreply.github.com>
2024-06-06 22:21:45 +0900

    Add files via upload

---
+++ MMM_SW/back/config/jwtMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const secret_config = require("./secret");
+const jwtMiddleware = function (req, res, next) {
+
+  const token = req.headers["x-access-token"] || req.query.token;
+  if (!token) {
+    return res.status(403).json({
+      isSuccess: false,
+      code: 403,
+      message: "로그인이 되어 있지 않습니다.",
+    });
+  }
+
+  try {
+    const verifiedToken = jwt.verify(token, secret_config.jwtsecret);
+    req.verifiedToken = verifiedToken;
+    next();
+  } catch {
+    res.status(403).json({
+      isSuccess: false,
+      code: 403,
+      message: "검증 실패",
+    });
+  }
+};
+
+module.exports = jwtMiddleware;
